d3aff880224dc01f2e9e2ec2c6fef36a9a90afa1
qwq233 <qwq233@qwq2333.top>
2025-06-13 21:39:04 +0800

    fix: disable resetprop security_patch by default
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- module/template/service.sh
+++ module/template/service.sh
@@ -4,11 +4,8 @@ MODDIR=${0%/*}
 
 cd $MODDIR
 
-resetprop "ro.build.version.security_patch" "2025-02-05"
-
 (
 while [ true ]; do
-  resetprop "ro.build.version.security_patch" "2025-02-05"
   ./daemon
   if [ $? -ne 0 ]; then
     exit 1
